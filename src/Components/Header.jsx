b9e12d9d3676a057bd11247bffbbbf8d73f683ad
NataliaArango <92561847+NataliaArango@users.noreply.github.com>
2021-10-24 23:19:51 -0500

    Actualización del Header
    En esta actualización, agregue el header de la ventana principal, aun le falta el logo, esta en realidad no tiene fondo, se camufla dentro de la foto que viene luego

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Header = () => {
     return ( 
-        <h2> Desde Header</h2>
+        <nav class=" j navbar navbar-expand-lg">
+              
+              <div class="container">
+                
+                <a class="navbar-brand" href="index.html">
+                </a>
+                <div class="todo">
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    
+                        <div class="navbar-nav " >
+                          <a class="nav-link" href="nosotros.html">Nosotros</a>
+                          <a class="nav-link" href="menu.html">Menu</a>
+                          <a class="nav-link" href="servicios.html">Servicios</a>
+                          <a class="nav-link" href="contactenos.html">Contactenos</a>
+                        </div>
+                    </div>
+                </div>
+          
+              </div>
+              
+            </nav>
      );
 }
  
