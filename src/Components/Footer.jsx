9a495933505355c6ee856f96a847e6ecf60de97c
NataliaArango <92561847+NataliaArango@users.noreply.github.com>
2021-10-24 21:06:13 -0500

    K

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,6 +17,25 @@ const Footer = () => {
                             </ul>
                         </div>
                     </div>
+                         
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+                        <ul class="list-unstyled list-inline social text-center">
+                            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                        </ul>
+                    </div>
+                </div>	
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center ">
+                        <p>Restaurante Sal y Salsa Ltda. Direccion: Cl. 10 ##37, Cali. Reservas: reservas@salysalsa.com. Tel: 3125672354</p>
+                        <p class="h6">©Copyright 2021.<a class="text-green ml-2" href="https://talento.juventic.co/" target="_blank">Juventic</a></p>
+                    </div>
+                </div>	
+                
                 </div>
         </Fragment>
 
